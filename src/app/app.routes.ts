192872e3a5f918e4cb31819f005ae376f2382df9
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2025-12-17 15:16:09 -0500

    Create dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import {UsersListComponent} from './users/users-list/users-list';
 import {UserProfileComponent} from './users/user-profile/user-profile';
 import {CallbackComponent} from './callback/callback.component';
 import {NotFoundComponent} from './not-found/not-found';
+import {DashboardComponent} from './dashboard/dashboard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'recipes', component: RecipesListComponent },
   { path: 'recipes/:id', component: RecipeDetailComponent },
   { path: 'ingredients', component: IngredientsListComponent },
