85440ff9b5ecb3df38e4341db14f6a33279c57c3
Spencer Perkins <95900100+SpecialThing44@users.noreply.github.com>
2025-12-17 14:01:39 -0500

    Add not found page

---
+++ src/app/not-found/not-found.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
+  templateUrl: './not-found.html',
+  styleUrl: './not-found.scss'
+})
+export class NotFoundComponent {}
